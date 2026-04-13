cc8450496a059ab57ed29f521746ee2203259f6c
Vittorio Pisapia <pisapia.vit@gmail.com>
2026-04-13 11:34:59 +0900

    revert rewards

--- legged_gym/envs/go2/go2_config.py
+++ legged_gym/envs/go2/go2_config.py
@@ -44,10 +44,10 @@ class GO2RoughCfg( LeggedRobotCfg ):
         class scales( LeggedRobotCfg.rewards.scales ):
             torques = -0.0002
             dof_pos_limits = -10.0
-            base_height = -0.1 #TODO now seems too low
+            base_height = -15 #TODO now seems too low
             feet_air_time = 1.5
             action_rate = -0.02
-            slip = -0.01    
+            slip = 0.0
 
 class GO2RoughCfgPPO( LeggedRobotCfgPPO ):
     class algorithm( LeggedRobotCfgPPO.algorithm ):
