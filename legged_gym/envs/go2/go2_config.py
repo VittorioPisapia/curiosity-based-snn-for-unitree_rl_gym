d37a2f0d155a62fba30f126389db3edf51eef767
Vittorio Pisapia <pisapia.vit@gmail.com>
2026-04-17 12:23:42 +0900

    minor changes rewards

--- legged_gym/envs/go2/go2_config.py
+++ legged_gym/envs/go2/go2_config.py
@@ -47,9 +47,9 @@ class GO2RoughCfg( LeggedRobotCfg ):
 
             # New
             base_height = -30 
-            feet_air_time = 0.1
+            feet_air_time = 0.01
             action_rate = -0.02
-            slip = -0.04
+            slip = -0.01
             cost_of_transport = -0.05 # -0.05 ~ -0.2
 
 class GO2RoughCfgPPO( LeggedRobotCfgPPO ):
