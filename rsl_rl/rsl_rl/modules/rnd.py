cb8c662350056a999966aed03b4c0e92f38f4f4b
Vittorio Pisapia <pisapia.vit@gmail.com>
2026-04-21 13:44:02 +0900

    added RND - not active

---
+++ rsl_rl/rsl_rl/modules/rnd.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .actor_critic import get_activation
+
+class RND(nn.Module):
+    def __init__(
+            self,
+            num_obs,
+            hidden_dim = 128,
+            feature_dim = 64,
+            activation = "relu"):
+        
+        super(RND, self).__init__()
+
+        target_layers = []
+        target_layers.append(nn.Linear(num_obs, hidden_dim))
+        target_layers.append(get_activation(activation))
+        target_layers.append(nn.Linear(hidden_dim, feature_dim))
+
+        self.target_model=nn.Sequential(*target_layers)  # FIXED
+
+        predictor_layers = []
+        predictor_layers.append(nn.Linear(num_obs, hidden_dim))
+        predictor_layers.append(get_activation(activation))
+        predictor_layers.append(nn.Linear(hidden_dim, feature_dim))
+
+        self.predictor_model=nn.Sequential(*predictor_layers)  # TRAINABLE
+
+        # Fixing parameters for target model
+        for p in self.target_model.parameters():
+            p.requires_grad = False
+
+    def forward(self, obs):
+        with torch.no_grad():
+            target_feat = self.target_model(obs)
+        
+        pred_feat = self.predictor_model(obs)
+
+        return pred_feat, target_feat
