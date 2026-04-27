d699fb9eaf2590aed193ac48a86cf6cd4797ec82
VittorioPisapia <VittorioPisapia@s13.minato>
2026-04-27 12:00:55 +0900

    added neurons number

--- legged_gym/envs/go2/go2_icm_config.py
+++ legged_gym/envs/go2/go2_icm_config.py
@@ -8,7 +8,7 @@ class GO2RoughICMCfgPPO( GO2RoughSNNCfgPPO):
             icm_beta = 0.3
             icm_intrinsic_coeff = 0.02
             icm_reward_clamp = 0.05
-            use_icm = False
+            use_icm = True
             use_rnd = False
             rnd_intrinsic_coeff = 0.007
             rnd_reward_clamp = 0.05
--- legged_gym/envs/go2/go2_snn_config.py
+++ legged_gym/envs/go2/go2_snn_config.py
@@ -5,10 +5,11 @@ class GO2RoughSNNCfgPPO( GO2RoughCfgPPO):
     runner_class_name = "SnnRunner"
     class policy ( GO2RoughCfgPPO.policy ):
         class snn:
-            snn_threshold = 0.5
-            snn_lens = 0.3
-            snn_st = 1
+            snn_threshold = 0.5 # 0.5
+            snn_lens = 0.3 # 0.3 Gaussian width
+            snn_st = 1  # 1
             neuron_type = "Gaussian" # Gaussian, BPTT
+            num_neurons = 384 # 256, 384, 512
 
     class runner ( GO2RoughCfgPPO.runner ):
         policy_class_name = "ActorCriticSNN"
