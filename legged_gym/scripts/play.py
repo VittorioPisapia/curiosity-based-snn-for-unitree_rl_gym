d099709bef6316f84f87f7170d1d653751923d3b
Vittorio Pisapia <pisapia.vit@gmail.com>
2026-05-12 10:08:30 +0900

    clean play file

--- legged_gym/scripts/play.py
+++ legged_gym/scripts/play.py
@@ -32,6 +32,11 @@ def play(args):
         env_cfg.env.num_envs = min(env_cfg.env.num_envs, 100)
     
     command_mode = args.cmd_type
+    timestamp = datetime.now().strftime('%b%d_%H-%M-%S')
+
+    robot_idx = 0 
+    logger = RobotLogger()
+    recorder = None
 
     env_cfg.terrain.num_rows = 5
     env_cfg.terrain.num_cols = 5
@@ -43,10 +48,8 @@ def play(args):
     env_cfg.domain_rand.max_push_vel_xy=1
 
     configure_commands(env_cfg, command_mode)
-    timestamp = datetime.now().strftime('%b%d_%H-%M-%S')
 
     env_cfg.env.test = True
-
 
     if hasattr(env_cfg.env, "enable_camera_sensors"):
         env_cfg.env.enable_camera_sensors = True
@@ -59,16 +62,12 @@ def play(args):
     ppo_runner, train_cfg = task_registry.make_alg_runner(env=env, name=args.task, args=args, train_cfg=train_cfg)
     policy = ppo_runner.get_inference_policy(device=env.device)
 
-    robot_idx = 0 
-    logger = RobotLogger()
     
     # export policy as a jit module (used to run it from C++)
     if EXPORT_POLICY:
         path = os.path.join(LEGGED_GYM_ROOT_DIR, 'logs', train_cfg.runner.experiment_name, 'exported', 'policies')
         export_policy_as_jit(ppo_runner.alg.actor_critic, path)
         print('Exported policy as jit script to: ', path)
-
-    recorder = None
 
     if args.record:
 
@@ -77,28 +76,23 @@ def play(args):
             'logs',
             train_cfg.runner.experiment_name
         )
-
         model_path = get_load_path(
             root=experiment_root,
             load_run=args.load_run,
             checkpoint=args.checkpoint
         )
-
         experiment_dir = os.path.dirname(model_path)
-
         video_dir = os.path.join(experiment_dir, 'videos')
         os.makedirs(video_dir, exist_ok=True)
-
         video_path = os.path.join(
             video_dir,
             f"{timestamp}_seed_{env_cfg.seed}.mp4"
         )
-
         recorder = VideoRecorder(env, video_path)
 
     try:
         for _ in range(700):
-        #for i in range(10*int(env.max_episode_length)):
+
             actions = policy(obs.detach())
             obs, _, rews, dones, infos = env.step(actions.detach())
             env.gym.refresh_rigid_body_state_tensor(env.sim)
@@ -116,36 +110,27 @@ def play(args):
     if logger.has_data():
 
         print(f"Plotting {logger.num_steps} steps of simulation...")
-
         fig = plot_run(logger, env, env_cfg)
 
         if args.plot:
-
             experiment_root = os.path.join(
                 LEGGED_GYM_ROOT_DIR,
                 'logs',
                 train_cfg.runner.experiment_name
             )
-
             model_path = get_load_path(
                 root=experiment_root,
                 load_run=args.load_run,
                 checkpoint=args.checkpoint
             )
-
             experiment_dir = os.path.dirname(model_path)
-
             plots_dir = os.path.join(experiment_dir, 'plots')
-
             os.makedirs(plots_dir, exist_ok=True)
-
             plot_path = os.path.join(
                 plots_dir,
                 f"{timestamp}_seed_{env_cfg.seed}.png"
             )
-
             fig.savefig(plot_path, dpi=300)
-
             print(f"Plots saved in : {plot_path}")
 
         show_plot()
